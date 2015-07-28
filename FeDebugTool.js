29fdf1c581fcdb3ee2c28f7949400e5e9cfafaf7
zhangchile <zzlchile@gmail.com>
2015-07-28 18:06:07 +0800

    添加断线重连功能。

--- FeDebugTool.js
+++ FeDebugTool.js
@@ -8,45 +8,47 @@ var FeDebugTool = {
 		this.host = host;
 		this.port = port;
 		this.url  = 'ws://'+host+':'+port+'/'; 
-		this.ws = new WebSocket(this.url,'echo-protocol');  //建于服务器之间的连接通信
+		this.ws = new WebSocket(this.url, 'echo-protocol');  //建于服务器之间的连接通信
 
 		var that = this;
 		//错误处理
 		window.onerror = function(errorMessage, scriptURI, lineNumber, columnNumber, error) {
 
-				if ((that.ws).readyState === that.ws.OPEN) {
-					//执行时的错误
-				    that.reportError({
-				    	message: errorMessage,
-				    	script: scriptURI,
-				    	line: lineNumber,
-				    	column: columnNumber
-				    });
-				} else {
-					//在页面加载时的错误
-					var str = that.createErrStr({
-				    	message: errorMessage,
-				    	script: scriptURI,
-				    	line: lineNumber,
-				    	column: columnNumber
-				    });
-					var interws = new WebSocket(that.url, 'echo-protocol');
-					interws.onopen = function() {
-				    	interws.send("error#" + str);
-					}
-					interws.onmessage = function(msg) {
-						// console.log(msg.data)
-				    	interws.close();
-					}
-					interws.onclose = function() {
-
-					}
+			if ((that.ws).readyState === that.ws.OPEN) {
+				//执行时的错误
+			    that.reportError({
+			    	message: errorMessage,
+			    	script: scriptURI,
+			    	line: lineNumber,
+			    	column: columnNumber
+			    });
+			} else {
+				//在页面加载时的错误
+				var str = that.createErrStr({
+			    	message: errorMessage,
+			    	script: scriptURI,
+			    	line: lineNumber,
+			    	column: columnNumber
+			    });
+				var interws = new WebSocket(that.url, 'echo-protocol');
+				interws.onopen = function() {
+			    	interws.send("error#" + str);
+			    	interws.close();
 				}
+				interws.onmessage = function(msg) {
+					// console.log(msg.data)
+			    	// interws.close();
+				}
+				interws.onclose = function() {
+
+				}
+			}
 			return true;
 		}
 
 
 		this.ws.onopen = function() {//通过onopen事件句柄来监听socket的打开事件
+			console.log('连接成功');
 		  	//发送客户端的信息
 		    var clientinfo = window.navigator.appName +","+ window.navigator.appVersion +","+ window.navigator.platform;
 		    // console.log(clientinfo)
@@ -56,18 +58,28 @@ var FeDebugTool = {
 		 
 		   var msg = e.data; 
 		   console.log("from server: "+ e.data)
-		  //  var chatBox = $('chat-box');  
-		  // // audioElement.play();      
-		  //  chatBox.innerHTML = chatBox.innerHTML+msg+'<br/>';
-		   if (msg.indexOf("command#") >= 0) {
+		   var chatBox = document.getElementById('chat-box');     
+		   chatBox.innerHTML = chatBox.innerHTML+msg+'<br/>';
+		    if (msg.indexOf("command#") >= 0) {
 		   		var command = msg.substr(msg.indexOf("#") + 1);
 		   		//创建script标签
 		   		var script = document.createElement('script'); 
 		   		script.type = 'text/javascript';
 		   		script.innerHTML = command;
 		   		document.body.appendChild(script); 
-		   }
-		}  
+		    }
+		}
+
+		this.ws.onclose = function() {
+			//断线重连
+			console.log('连接已断开，3秒后尝试重连...');
+			setTimeout(function() { that.reconect(that.host, that.port)}, 3000);
+		}
+
+		this.ws.onerror = function() {
+			//错误
+			console.log('连接失败，无法连接服务器，请检查服务器配置是否正确');
+		}
 		    
 		function send() { //使用send方法向服务器发送数据
 		
@@ -78,47 +90,52 @@ var FeDebugTool = {
 
 	},
 
-		createErrStr: function (error) {
-			return "错误信息：" + error.message + "，文件：" + error.script + " 第" + error.line +"行，" + error.column + "列";
-		},
+	createErrStr: function (error) {
+		return "错误信息：" + error.message + "，文件：" + error.script + " 第" + error.line +"行，" + error.column + "列";
+	},
 
+	reportError: function (error) {
+		// console.log(this.ws.readyState)
 
-		reportError: function (error) {
-			// console.log(this.ws.readyState)
+		var errorStr = this.createErrStr(error);
 
-			var errorStr = this.createErrStr(error);
+		// console.log(errorStr);
 
-			// console.log(errorStr);
+		this.ws.send("error#" + errorStr); 
 
+	},
+
+	recordError: function (error) {
+		if(window.error_log == undefined) {
+			window.error_log = [];
+		}
+		var errorStr = this.createErrStr(error);
+		// console.log(errorStr);
+		window.error_log.push(errorStr);
+		// console.log(error_log)
+	},
+
+	sleep: function (milliSeconds) {
+		var startTime = new Date().getTime();
+		while (new Date().getTime() < startTime + milliSeconds);
+	},
+
+	sendRecord: function () {
+		// alert('df')
+		for (var i = 0; i < window.error_log.length - 1; i++) {
+
+			var errorStr = createErrStr(error);
 			this.ws.send("error#" + errorStr); 
+			console(errorStr);
+		};
+	},
+	debugDump: function(value) {
+		this.ws.send("debug#>" + value.toString());
+	},
 
-		},
+	reconect : function (host, port) {
+		this.init(host,port);
+	}
 
-		recordError: function (error) {
-			if(window.error_log == undefined) {
-				window.error_log = [];
-			}
-			var errorStr = this.createErrStr(error);
-			// console.log(errorStr);
-			window.error_log.push(errorStr);
-			// console.log(error_log)
-		},
-
-		sleep: function (milliSeconds) {
-			var startTime = new Date().getTime();
-			while (new Date().getTime() < startTime + milliSeconds);
-		},
-
-		sendRecord: function () {
-			// alert('df')
-			for (var i = 0; i < window.error_log.length - 1; i++) {
-
-				var errorStr = createErrStr(error);
-				this.ws.send("error#" + errorStr); 
-				console(errorStr);
-			};
-		},
-		debugDump: function(value) {
-			this.ws.send("debug#" + value.toString());
-		}	
 }
+
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-//当前连接的客户端
-var clients = Array();
+//连接的设备数
+var _device = [];
+//当前的连接数
+var _clients = [];
 //控制台
 var front_console = null;
 
@@ -12,8 +14,8 @@ var WebSocketServer = require('websocket').server;
 var http = require('http');
 var url = require('url');
 
-
 function start(route, handle) {
+
     function onRequest(request, response) {
         var postData = "";
         var pathname = url.parse(request.url).pathname;
@@ -29,102 +31,107 @@ function start(route, handle) {
         });
     }
 
+    var server = http.createServer(onRequest);
+    server.listen(webSocketsServerPort, function() {
+        console.log(getNow() + " WebSocket Server is listening on port：" + webSocketsServerPort);
+    });
 
-var server = http.createServer(onRequest);
-server.listen(webSocketsServerPort, function() {
-    console.log(getNow() + " WebSocket Server is listening on port：" + webSocketsServerPort);
-});
+    var wsServer = new WebSocketServer({httpServer: server});
 
-var wsServer = new WebSocketServer({httpServer: server});
+    wsServer.on('request', function(request) {
+        // console.log(getNow() + ' -- ' + request.origin + ' 请求连接.');
 
-wsServer.on('request', function(request) {
-    // console.log(getNow() + ' -- ' + request.origin + ' 请求连接.');
+        var connection = request.accept('echo-protocol', request.origin); 
+        var index = _clients.push(connection) - 1;
+        console.log(getNow() + " " + request.origin + ' 已建立连接...');
 
-    var connection = request.accept('echo-protocol', request.origin); 
-    var index = clients.push(connection) - 1;
-    console.log(getNow() + " " + request.origin + ' 已建立连接...');
+        connection.on('message', function(message) {
+            if(message.type === 'utf8') {
+                // console.log(getNow() + ': ' + message.utf8Data);
 
-     connection.on('message', function(message) {
-        if(message.type === 'utf8') {
-            // console.log(getNow() + ': ' + message.utf8Data);
+                //信息过滤与分派
+                var msg = message.utf8Data;
 
-            //信息过滤与分派
-            var msg = message.utf8Data;
+                if (msg == "console") {
+                    console.log(getNow() + " 控制台已连接。")
+                    front_console = connection;
+                    connection.sendUTF("msg#控制台已连接。");
+                    _clients.pop();
+                    index = index - 1;
+                } else if (msg.substr(0, 8) == "command#") {
+                    //来自控制台的命令
+                    var command = msg.substr(msg.indexOf("#") + 1);
+                    console.log(command);
+                    front_console.sendUTF("msg#执行命令：" + command);
 
-            if (msg == "console") {
-                console.log(getNow() + " 控制台已连接。")
-                front_console = connection;
-                connection.sendUTF("控制台已连接。");
-                clients.pop();
-                index = index - 1;
-            } else if (msg.substr(0, 8) == "command#") {
-                //来自控制台的命令
-                var command = msg.substr(msg.indexOf("#") + 1);
-                console.log(command);
-                front_console.sendUTF("执行命令：" + command);
-
-                for (var i = clients.length - 1; i >= 0; i--) {
-                    clients[i].sendUTF(msg);
-                };
-            } else if (msg.substr(0, 6) == "error#") {
-                var error = msg.substr(msg.indexOf("#") + 1);
-                if (front_console) {
-                    front_console.sendUTF("<span class='error'>" + error + "</span>");
+                    for (var i = _clients.length - 1; i >= 0; i--) {
+                        _clients[i].sendUTF(msg);
+                    };
+                } else if (msg.substr(0, 6) == "error#") {
+                    var error = msg.substr(msg.indexOf("#") + 1);
+                    if (front_console) {
+                        front_console.sendUTF("msg#<span class='error'>" + error + "</span>");
+                    }
+                    //如果控制台没连接
+                } else if (msg.substr(0, 6) == "debug#") {
+                    var debug = msg.substr(msg.indexOf("#") + 1);
+                    if (front_console) {
+                        front_console.sendUTF('msg#'+debug);
+                    }
+                } else if (msg.substr(0, 7) == "client#"){
+                    //客户端连接 发送客户端的信息
+                    var client = msg.substr(msg.indexOf("#") + 1);
+                    var devi = {
+                        appName    : client.split(",")[0],
+                        appVersion : client.split(",")[1],
+                        platform   : client.split(",")[2],
+                    };
+                    _device.push(devi);
+                    if (front_console) {
+                        front_console.sendUTF("msg#客户端：" + devi.platform + " 已建立连接，ip："+request.origin);
+                        console.log("客户端：" + devi.platform + " 已建立连接，ip："+request.origin);
+                    }
+                } else {
+                    //default
+                    front_console.sendUTF(message.utf8Data);
                 }
-                //如果控制台没连接
-            } else if (msg.substr(0, 6) == "debug#") {
-                var debug = msg.substr(msg.indexOf("#") + 1);
-                if (front_console) {
-                    front_console.sendUTF(debug);
-                }
-            } else if (msg.substr(0, 7) == "client#"){
-                //客户端连接 发送客户端的信息
-                var client = msg.substr(msg.indexOf("#") + 1);
-                var appName = client.split(",")[0];
-                var appVersion = client.split(",")[1];
-                var platform = client.split(",")[2];
-                if (front_console) {
-                    front_console.sendUTF("客户端："+platform + " 已建立连接");
-                }
+
             } else {
-                //
-                front_console.sendUTF(message.utf8Data);
+                //message type not utf8
+                //do something
             }
+        });
 
-        }
-     });
-
-    connection.on('close', function(connection) {
-            console.log(getNow() + " -- " + connection.remoteAddress + " 断开链接.");
-            clients.pop();
+        connection.on('close', function(connection) {
+            console.log(getNow() + " -- " + request.origin + " 断开链接.");
+            _clients.pop();
             index = index - 1;
-            console.log(getNow() + " 当前连接数：" + clients.length);
+            console.log(getNow() + " 当前连接数：" + _clients.length);
             if (front_console) {
-                front_console.sendUTF(getNow() + " -- " + connection.remoteAddress + " 断开链接。当前连接数："+ clients.length);
+                front_console.sendUTF("command#setClient("+_clients.length+")");
             }
         });
     });
 
+    function getNow() {
+        return new Date().format('yyyy-MM-dd hh:mm:ss');
+    }
 
-function getNow() {
-    return new Date().format('yyyy-MM-dd hh:mm:ss');
-}
-
-Date.prototype.format = function (fmt) { //author: meizz   
-     var o = {
-         "M+": this.getMonth() + 1,
-         "d+": this.getDate(),   
-         "h+": this.getHours(),   
-         "m+": this.getMinutes(),  
-         "s+": this.getSeconds(),   
-         "q+": Math.floor((this.getMonth() + 3) / 3),   
-         "S": this.getMilliseconds()   
-     };
-     if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-     for (var k in o)
-     if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-     return fmt;
- }
+    Date.prototype.format = function (fmt) { //author: meizz   
+        var o = {
+            "M+": this.getMonth() + 1,
+            "d+": this.getDate(),   
+            "h+": this.getHours(),   
+            "m+": this.getMinutes(),  
+            "s+": this.getSeconds(),   
+            "q+": Math.floor((this.getMonth() + 3) / 3),   
+            "S": this.getMilliseconds()   
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    }
 
 }
 
